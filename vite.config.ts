4ef424c6feae907435d201aaa5746e0dc614cfc3
lvhaizhou <417703682@qq.com>
2022-10-20 21:24:31 +0800

    配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { vitestConfig } from "./vitestConfig"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  test: {
+    ...vitestConfig
+  },
   plugins: [vue()]
 })
